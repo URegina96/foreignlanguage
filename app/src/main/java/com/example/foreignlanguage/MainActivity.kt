522d3abef8edd5fb019281b3d7db9fc56b2c572b
regina <usmanova.re@yandex.ru>
2024-03-09 14:42:16 +0500

    Работа BottomNavigationView
    Установка слушателей на item для перехода по фрагментам разделов через основное меню

--- app/src/main/java/com/example/foreignlanguage/MainActivity.kt
+++ app/src/main/java/com/example/foreignlanguage/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.foreignlanguage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.lessonsFragment -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.lessonsFragment)
+                    true
+                }
+                R.id.exercisesFragment -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.exercisesFragment)
+                    true
+                }
+                R.id.dictionaryFragment -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.dictionaryFragment)
+                    true
+                }
+                R.id.settingsFragment -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.settingsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
