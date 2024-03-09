c48dc429968c04faf9bd2bd045c12d818887574e
regina <usmanova.re@yandex.ru>
2024-03-09 14:36:52 +0500

    Работа с navigation, раздел dictionary
    Добавление и описание фрагмента dictionary

---
+++ app/src/main/java/com/example/foreignlanguage/dictionary/DictionaryFragment.kt
@@ -0,0 +1,30 @@
+package com.example.foreignlanguage.dictionary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.foreignlanguage.R
+
+class DictionaryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dictionary, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.buttonEnglishDictionaryFragment).setOnClickListener {
+            findNavController().navigate(R.id.action_dictionaryFragment_to_englishDictionaryFragment)
+        }
+        view.findViewById<Button>(R.id.buttonJapaneseDictionaryFragment).setOnClickListener {
+            findNavController().navigate(R.id.action_dictionaryFragment_to_japaneseDictionaryFragment)
+        }
+    }
+
+}
